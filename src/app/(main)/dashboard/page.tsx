becbed139866862dee185028fb23d7757d4778ee
hendratanjaya <hendratanjaya1@gmail.com>
2025-10-26 15:39:20 +0700

    [UPDATE] add main layout for the whole page

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,15 +1,8 @@
 "use client";
-import { logoutAction } from "@/app/_actions/action";
-import { useActionState } from "react";
-
 export default function Page() {
-  const [_, formAction, __] = useActionState(logoutAction, null);
   return (
-    <div className="flex w-full justify-between bg-primary">
+    <div className="flex-1 w-full justify-between bg-primary rounded-md">
       <div>ini di main</div>
-      <form action={formAction}>
-        <button type="submit">Log out</button>
-      </form>
     </div>
   );
 }

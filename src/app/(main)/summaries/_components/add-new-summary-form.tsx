9430c9f36ca3f8400e96448960f53674c4a48f2f
hendratanjaya <hendratanjaya1@gmail.com>
2025-12-22 10:55:32 +0700

    [UPDATE] fix redirect url when creating new summaries

--- src/app/(main)/summaries/_components/add-new-summary-form.tsx
+++ src/app/(main)/summaries/_components/add-new-summary-form.tsx
@@ -70,7 +70,7 @@ export function AddNewSummaryForm({
       // To Do render generated course into the dedicated page
 
       const courseId = state.field;
-      router.push(`summarize/${courseId}`);
+      router.push(`/summaries/${courseId}`);
     });
   };
 
